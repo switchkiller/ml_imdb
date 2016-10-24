5e883fa7ef2714f9f6f8abbdb183df7f6ee19402
switchkiller <emailbarung@gmail.com>
2016-10-24 17:32:09 +0530

    Implemented Naive Bayes Algo benchmark: 67%

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,9 +1,11 @@
 import pandas as pd
 train_data = pd.read_csv('../data/labeledTrainData.tsv', sep='\t')
+test_data = pd.read_csv('../data/testData.tsv', sep='\t')
 
 import matplotlib.pyplot as plt
 
 train_data['review_length'] = train_data.review.apply(len)
+test_data['review_length'] = test_data.review.apply(len)
 # p = plt.hist(train_data.review_length.values)
 # plt.show()
 
@@ -24,26 +26,29 @@ from sklearn.feature_extraction.text import CountVectorizer
 vocab= ['awesome', 'good', 'amazing', 'interesting', 'terrible', 'bad', 'awful','boring']
 simple_vectorizer = CountVectorizer(vocabulary = vocab)
 bow = simple_vectorizer.fit_transform(train_data.review).todense()
-
-print (bow)
-
-
+bow_test = simple_vectorizer.fit_transform(test_data.review).todense()
+# print (bow)
 
 words = list(simple_vectorizer.vocabulary_.keys())
 print (words)
 
 
 df = pd.DataFrame(bow, index = train_data.index, columns = words )
+df_test = pd.DataFrame(bow_test, index = test_data.index, columns=words)
 # print(df)
 
 # ll = df.words.values
 p = plt.hist([df.awesome, df.good, df.amazing, df.interesting, df.terrible, df.bad, df.awful, df.boring], label = words)
-plt.legend()
-plt.show()
+# plt.legend()
+# plt.show()
 
 # val = df[(df.awesome==0) & (df.terrible==0)].count()
 
 # print(val)
 df_expand = train_data.join(df)
+df_expand_test = test_data.join(df_test)
+
+# print(df_expand_test)
 
 df_expand.to_csv('../save/trainDataFeatures.tsv', sep='\t')
+df_expand_test.to_csv('../save/testDataFeatures.tsv', sep='\t')
--- src/modelling.py
+++ src/modelling.py
@@ -1,5 +1,21 @@
-import pandas as import pd
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-train_data = pd.read_csv('../save/trainDataFeatures.tsv', sep='\t')
-print ()
+df = pd.read_csv('../save/trainDataFeatures.tsv', sep='\t', index_col = 0)
+test_df = pd.read_csv('../save/testData.tsv', sep='\t', index_col=0)
+# df.drop(['review'], axis=1, inplace=True)
+columns = df.columns[3:]
+print (columns)
+
+X = np.asarray(df[columns])
+y = np.asarray(df.sentiment.transpose())
+
+# Using Naive Bayes Classifier
+from sklearn.naive_bayes import MultinomialNB
+clf = MultinomialNB()
+clf.fit(X,y)
+
+clf.predict(np.asarray(df[columns]))
+
+mean = np.mean(clf.predict(np.asarray(df[columns])) == df.sentiment)
+print (mean)
